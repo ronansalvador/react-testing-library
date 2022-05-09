57d4f91aa7163830969c6c691fcc993ce408e58b
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-05-09 18:56:00 -0300

    Corrigindo Eslinst Req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
-// import App from '../App';
 
 describe('Teste o componente <About.js />', () => {
   it('Teste se a página contém as informações sobre a Pokédex.', () => {

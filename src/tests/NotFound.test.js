d6501101d93e0d337d9e20b3829fb66a8aeff5be
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-05-09 19:48:47 -0300

    Req 4 - NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+describe('', () => {
+  it('', () => {
+    renderWithRouter(<NotFound />);
+    const titleEl = screen.getByRole('heading',
+      { name: /page requested not found/i, level: 2 });
+    expect(titleEl).toBeDefined();
+    const image = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+    expect(image).toBeDefined();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    screen.logTestingPlaygroundURL();
+  });
+});
